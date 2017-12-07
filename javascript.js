d335b56964734f21db552afe49206aab61360bb3
Eric Oeur <ericeoeur@gmail.com>
2017-12-07 00:10:19 -0600

    Final Version for Gifastic Pokemon Edition

--- javascript.js
+++ javascript.js
@@ -1,45 +1,49 @@
-
 var pokemonList = ["Raichu", "Marowak", "Togepi", "Mudkip", "Meowth", "Koffing", "Ekans", "Torchic", "Gengar"];
 
 function displayPokemon() {
 
     let pokemon = $(this).attr("pokemon-name");
-    //let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+pokemon; //random photo
-    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + pokemon;  //search query 
+    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + pokemon;  //search query
 
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }) // After the data from the AJAX request comes back
-        .done(function (response) {
+        
+    .done(function (response) {
             console.log(response);
+            $("#pokemon").html("");
 
+        
+        //Make a for loop to display all images
             for (i = 0; i < 10; i++) {
-
-                $("#pokemon").prepend(
-                    "<p>Rated " + response.data[i].rating + "</p>"
-                );
-
+                //set variables with still and moving image from giphy
                 let stillImageUrl = response.data[i].images.fixed_height_still.url;
                 let movingImageUrl = response.data[i].images.fixed_height.url;
                 let pokeImage = $("<img>");
-                //let arrange = $('<div class="row"><div class="col-sm-6 col-md-4"><div class="thumbnail">');
-                pokeImage.attr("src", stillImageUrl);
-                pokeImage.attr("alt", "nonMoving");
-                pokeImage.attr("id", "stillImage");
-                pokeImage.attr("class", "col-sm-4");
-               //$("#pokemon").prepend('<div class="row"><div class="col-sm-6 col-md-4"><div class="thumbnail">');
-                $("#pokemon").prepend(pokeImage);
-                //$("#pokemon").prepend('</div></div></div>');
+                    pokeImage.attr("src", stillImageUrl);
+                    pokeImage.attr("alt", "nonMoving");
+
+                //add rating information per image; include in for loop's [i]
+                let rating = $("<p>Rated " + response.data[i].rating + "</p>");
+                let col = $("<div>");
+                    col.attr("class", "pokeImage");
+                    col.append(rating);
+
+                //set thumbnail bootstrap with divs 
+                let thumbnail = $("<div>");
+                    thumbnail.attr("class", "thumbnail");
+                    thumbnail.append(pokeImage);
+
+                col.append(thumbnail);
+
+                //append all information from div to pokemon id 
+                $("#pokemon").append(col);
 
 
-             
-                
-
-                $("#stillImage").on("click", function () {
-
-
+                //set on click event for moving/still images
+                pokeImage.on("click", function () {
                     let stillGif = (stillImageUrl);
                     let movingGif = (movingImageUrl);
 
@@ -81,14 +85,13 @@ function renderPokemon() {
     }
 }
 
-
-// This function handles events when the add movie button is clicked
+// This function handles events when the add pokemon button is clicked
 $("#addPokemon").on("click", function (event) {
     event.preventDefault();
-    var pokemon = $("#pokemon-input").val().trim(); // This line of code will grab the input from the textbox
-    pokemonList.push(pokemon);// The movie from the textbox is then added to our array
-    renderPokemon(); // Calling renderButtons which handles the processing of our movie array into a button
+    var pokemon = $("#pokemon-input").val().trim(); // This line of code will grab the input from the pokemon-input text box
+    pokemonList.push(pokemon);// The pokemon from the is added to array
+    renderPokemon(); // Calling renderPokemon which handles the processing of our pokemon array into a button
 });
 
 $(document).on("click", ".pokemon", displayPokemon);
-renderPokemon(); 
+renderPokemon();
